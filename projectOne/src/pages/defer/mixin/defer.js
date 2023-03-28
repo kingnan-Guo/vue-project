c95d1ccef78d16bc8ecbbd9959468b2de7ff6b62
kingnan <373734199@qq.com>
2023-03-28 11:59:59 +0800

    更新代码

--- projectOne/src/pages/defer/mixin/defer.js
+++ projectOne/src/pages/defer/mixin/defer.js
@@ -1,34 +1,32 @@
 /**
  * 使用延迟 装载  多div 渲染（defer）
- * @param {*} maxFrameCount 最大渲染树 
- * @returns 
- * 
+ * @param {*} maxFrameCount 最大渲染树
+ * @return
  * JS 传输完成后  浏览器开始执行JS 构造页面
  * 但有可能一开始渲染组件过多，不仅JS执行的时间长，而且执行完后 浏览器要渲染的元素的过程中导致白屏
- * 
  * 一个可行的方法就是延迟装载组件，让组件按照指定的先后顺序依次一个一个渲染出来
  */
 export default function (maxFrameCount) {
   return {
-    data() {
-      return{
-        freameCount: 0,
+    data () {
+      return {
+        freameCount: 0
       }
     },
-    mounted() {
-      const refreashFrameCount = () =>{
+    mounted () {
+      const refreashFrameCount = () => {
         requestAnimationFrame(() => {
-          this.freameCount++;
+          this.freameCount++
           if (this.freameCount < maxFrameCount) {
-              refreashFrameCount();
+            refreashFrameCount()
           }
         })
       }
-      refreashFrameCount();
+      refreashFrameCount()
     },
-    methods:{
-      defer(showInFrameCount) {
-          return this.freameCount >= showInFrameCount
+    methods: {
+      defer (showInFrameCount) {
+        return this.freameCount >= showInFrameCount
       }
     }
   }
