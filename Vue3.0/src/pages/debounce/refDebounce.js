1a0b26a310976ab81048173c294c9bd9d0d9910d
kingnan <373734199@qq.com>
2023-03-29 20:40:17 +0800

    更新代码

--- Vue3.0/src/pages/debounce/refDebounce.js
+++ Vue3.0/src/pages/debounce/refDebounce.js
@@ -1,4 +1,14 @@
+/**
+ * customRef
+ * customRef 可以用来创建一个自定义的 ref，并对其依赖项跟踪和更新触发进行显式控制。它需要一个工厂函数，该函数接收 track 和 trigger 函数作为参数，并且应该返回一个带有 get 和 set 的对象。
+ */
 import { customRef } from "vue"
+/**
+ * 
+ * @param {*} value 传值
+ * @param {*} delay 延时时间
+ * @returns 
+ */
 export function refDebounce (value, delay = 1000) {
     console.log('ref value ==', value, delay)
     var time
@@ -23,15 +33,3 @@ export function refDebounce (value, delay = 1000) {
     })
 }
 
-// function ref(value) {
-//     {
-//         get () {
-//             return value
-//         },
-//         set (val) {
-//             value = val
-
-//         },
-
-//     }
-// }
