14708f410735d8bcb5119fccacecbeeade872c63
kingnan <373734199@qq.com>
2023-03-31 15:40:45 +0800

    更新代码结构

--- Vue3.0/src/router/index.js
+++ Vue3.0/src/router/index.js
@@ -1,15 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
  
-import debounce from '@/pages/debounce/debounce-page.vue'
+import debounce from '@/views/debounce/debounce-page.vue'
+
+import boradCastSend from '@/views/BoradCast/boradCastSend.vue'
+import boradCastReceive from '@/views/BoradCast/boradCastReceive.vue'
 
 const routes = [
   {
     //登录页面
-    path: "/login",
-    name: "login",
+    path: "/debounce",
+    name: "debounce",
     // component: () => import("@/page/debounce/index.vue"),
     component: debounce
   },
+  {
+    //登录页面
+    path: "/boradCast",
+    name: "boradCast",
+    component: boradCastSend
+  },
+  {
+    path: '/main', // 主屏
+    name: 'Main',
+    children: [
+      {
+        path: '/boradCastSend',
+        name: 'boradCast',
+        component: boradCastSend
+      },
+      {
+        path: '/boradCastReceive',
+        name: 'boradCastReceive',
+        component: boradCastReceive
+      }
+    ]
+  }
+
+
 ];
 const router = createRouter({
   history: createWebHistory(),
